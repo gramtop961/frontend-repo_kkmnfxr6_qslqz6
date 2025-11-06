0eaffd8a2342c04aa76ce1f95b82a24b48f9230d
Flames <vibe-coder@flames.com>
2025-11-06 02:44:40 +0000

    Update project prj_kkmnfxr6 - 2025-11-06T02:44:39.701Z

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
-import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
       <main>
         <Hero />
         <Projects />
-        <Contact />
       </main>
       <Footer />
     </div>
